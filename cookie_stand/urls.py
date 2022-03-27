f6759b3953ae74b8f523ad9b4d48f32fb1196ace
Wenhao Piao <wenhaopi@amazon.com>
2022-03-26 19:09:44 -0700

    Implement API via djangorestframework

---
+++ cookie_stand/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from .views import CookieStandList, CookieStandDetail
+
+
+urlpatterns = [
+    path('', CookieStandList.as_view(), name='cookiestand_list'),
+    path('<int:pk>/', CookieStandDetail.as_view(), name='cookiestand_detail')
+]
+
--- cookie_stand_project/settings.py
+++ cookie_stand_project/settings.py
@@ -42,6 +42,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'cookie_stand',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
